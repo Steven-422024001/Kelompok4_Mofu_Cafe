70892bbfa690e5b2fbff880c408c6c9edfbe0cef
Steven-422024001 <steven.422024001@civitas.ukrida.ac.id>
2025-10-14 14:24:12 +0700

    reuben

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,10 @@ use App\Http\Controllers\CategoryController;
 
 Route::get('/', [ProductController::class, 'index'])->name('home');
 
+// route untuk halaman produk
+Route::resource('products', ProductController::class);
+
+
 //route resource for products
 Route::resource('/products', \App\Http\Controllers\ProductController::class);
+Route::resource('category', App\Http\Controllers\CategoryController::class);
