24134551321f6a5165c22bc97769be50cb94eacb
Steven-422024001 <steven.422024001@civitas.ukrida.ac.id>
2025-10-14 17:03:42 +0700

    new

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,11 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\Supplier;
+
+class DashboardController extends Controller {
+    
+}
